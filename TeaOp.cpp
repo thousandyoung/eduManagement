ee88d159f628bbd4357cbb6f1d386c78f2ea7377
paopaochahu09 <79733950+paopaochahu09@users.noreply.github.com>
2021-03-05 23:14:22 +0800

    Update TeaOp.cpp

--- TeaOp.cpp
+++ TeaOp.cpp
@@ -9,7 +9,7 @@ double propotion;		//平时成绩比例
 string filename;
 //登录老师账号
 int TeaOp::Tea_Login(string name, string password) {
-	string real_password = openAccount<Teacher>(name, Stu);
+	string real_password = openAccount<Teacher>(name, Tea_number);
 	if (real_password == "\0")return -1;		//用户不存在
 	if (real_password == password)return 1;		//登录成功
 	return 0;									//密码错误
