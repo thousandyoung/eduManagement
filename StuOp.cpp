26604fe110fa48ff8227802f5c3eaeec8e69c8ae
thousandyoung <67034123+thousandyoung@users.noreply.github.com>
2021-03-06 22:58:43 +0800

    Update StuOp.cpp

--- StuOp.cpp
+++ StuOp.cpp
@@ -1,6 +1,6 @@
 #include"main.h"
 int StuOp::Stu_Login(string name,string password) {
-	string real_password = openAccount<Student>(name,Stu);
+	string real_password = openAccount<Student>(name,Stu_name);
 	if (real_password == "\0")return -1;		  //用户不存在
 	if (real_password == password)return 1;		//登录成功
 	return 0;									                //密码错误
@@ -8,6 +8,6 @@ int StuOp::Stu_Login(string name,string password) {
 
 vector<Student> StuOp:: Stu_ReadScore(string stu_number) {   //学生根据学号读成绩
     vector<Student> result;
-    copyInfo(stu_number, Stu, result);
+    copyInfo(stu_number, Stu_name, result);
     return result;
 }
