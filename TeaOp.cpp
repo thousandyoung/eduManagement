91ea405d785cef03038734b36162ca82a326a09e
thousandyoung <67034123+thousandyoung@users.noreply.github.com>
2021-03-06 23:11:21 +0800

    Update TeaOp.cpp

--- TeaOp.cpp
+++ TeaOp.cpp
@@ -1,121 +1,77 @@
-//
-//  edu.hpp
-//  4
-//
-//  Created by Takashii on 2021/3/4.
-//
-
 #include"main.h"
+#include<cstring>
+#include<sstream>
+string cou_name;       //课程名字
+string tea_name;       //授课老师姓名
+string position;       //授课地点
+string cou_time;          //授课时间
+double propotion;		//平时成绩比例
+string filename;
+//登录老师账号
+int TeaOp::Tea_Login(string number, string password) {
+	string real_password = openAccount<Teacher>(number, Stu_number);
+	if (real_password == "\0")return -1;		//用户不存在
+	if (real_password == password)return 1;		//登录成功
+	return 0;									//密码错误
+}
 
-#include <stdio.h>
-#include<iostream>
-#include<fstream>
-#include<string>
-#include<vector>
-#include<cstdlib>
-#include<map>
-#include"main.h"
-using namespace std;
+//将老师所教课程读出来
+vector<string>getTeaCourse(const Teacher& tea) {
+	return split(tea.course, "&");
+}
 
+//将课程中所有信息读出来
+vector<Course> TeaOp::Tea_ReadClassScore(string c_name) {
+	filename = "Teacher/" + c_name + ".dat";
+	char s[80];
+	fstream f(filename, ios::in);
+	f.getline(s,80);
+	vector<Course>classStu;
+	while ((long)f.tellg()!=ios::end)
+	{
+		Course temp;
+		f.read((char*)&temp, sizeof(temp));
+		classStu.push_back(temp);
+	}
+	return classStu;
+}
+//读出course文件第一行的课程信息
+void TeaOp::getInfo(string filename) {
+	char s[80];
+	fstream f(filename, ios::in|ios::binary);
+	f.getline(s, 80);
+	stringstream ss(s);
+	ss >> cou_name >> tea_name >> position >> cou_time >> propotion;
+}
 
-multimap<string, string>Tea_number;
-multimap<string, string>Tea_name;
-multimap<string, string>Ad_number;
-multimap<string, string>Ad_name;
+//通过学号计算并修改学生的总评成绩
+int TeaOp::InputScore(string number,double score1,double score2) {
+	vector<Student>s;
+	if (copyInfo(number, Stu_number, s)==0)return 0;
+	Course temp(s[0].name, s[0].number, to_string(score1), to_string(score2), to_string(score1 * propotion + (1 - propotion) * score2));
+	fstream f(filename, ios::binary | ios::out|ios::app);
+	f.write((char*)&temp, sizeof(temp));//修改课程文件
+	changeScore(s, score1 * propotion + (1 - propotion) * score2);
+	return 1;
+}
 
-
-    int Admin_Login(string number,vector<Admini> & x,string password)  //number是用户输入的工号  x是登陆成功后返回用户的个人信息
-    {
-        string real_password = openAccount<Admini>(number, Ad_number);   //这里的password是用户输入的密码,用教务员工号来查
-        if (real_password == "\0")return -1;		//用户不存在
-        else if (real_password == password)	//登录成功 登陆成功后把用户个人信息录入
-        {
-            copyInfo(number,Ad_number,x);
-            return 1;
-        }
-        else return 0;
-    }
-    int Admin_Modify()
-    {
-        //修改信息可以在前端的按键控制函数里写，直接把用户修改后厄对象写道类对象中，再通过modifyInfo()函数写到文件里
-    }
-    int Admin_Search()
-    {
-        int SearchStu();
-        int SearchTea();
-        int SearchCourse();
-        return 0;
-    };
-    int SearchStu_number(string number,vector<Student> & x)    //按学号搜索学生信息 前端用户输入的学号传入number对象中
-    {
-        //x存储学生信息
-        int flag=copyInfo(number,Ad_number,x);  //学生信息存在x中，供前端使用
-        return flag;   //flag为1表示搜索成功，反之失败
-    }
-    int SearchStu_name(string name,vector<Student> & x)      //按名字搜索学生名字    前端用户输入的姓名传入name对象中
-    {
-        int flag=copyInfo(name,Ad_name,x);  //学生信息存在x中，供前端使用
-        return flag;   //flag为1表示搜索成功，反之失败
-    }
-    int SearchTea_number(string number,vector<Teacher> &x)
-    {
-        int flag=copyInfo(number,Tea_number,x);
-        return flag;
-    }
-    int SearchTea_name(string name,vector<Teacher> &x)
-    {
-        int flag=copyInfo(name,Tea_name,x);
-        return flag;
-    }
-    int SearchCourse(string name,map<string,vector<Course>> &x,vector<string> & y)     //x中记录了课程文件名和课程信息的一一对应 y记录了所有课程文件名
-    {
-        fstream f("Course//"+name+"//record.dat",ios::binary|ios::in);
-        f.seekg(0,ios::end);
-        long End=f.tellg(); f.seekg(0,ios::beg);
-        for(;f.tellg()!=End;)
-        {
-            string temp;
-            f.read((char*)&temp,sizeof(string));     //将所有课程文件名读入y中
-            y.push_back(temp);
-        }
-        if(y.size()==0) return 0;     //没有课程班级
-        f.close();      //读取完毕，接下来再依次读取全部课程文件
-
-        for(int i=0;i<y.size();i++)
-        {
-            string temp="Course//"+name+"//"+y[i]+".dat";
-            vector<Course> temp_cou;
-            copyAllInfo(temp,temp_cou);    //将对应课程文件中信息全部读入temp_cou容器中，再保存入x中
-            x[name+"_"+y[i]]=temp_cou;  //读取完毕  key=“马克思主义实践_张萌5班"
-        }
-        return 0;
-    }
-    int multi_Search(string cou_name,string tea_name,map<string,vector<Course>> &x,vector<string> &y)  //在教务员搜索课程信息时，支持两个关键字————》课程名字 老师
-    {
-        fstream f("Course//"+cou_name+"//record.dat",ios::binary|ios::in);
-        f.seekg(0,ios::end);
-        long End=f.tellg(); f.seekg(0,ios::beg);
-        for(;f.tellg()!=End;)
-        {
-            string temp;
-            f.read((char*)&temp,sizeof(string));     //将所有课程文件名读入y中
-            y.push_back(temp);
-        }
-        if(y.size()==0) return 0;     //没有课程班级
-        f.close();
-
-        //开始从课程名字中寻找和老师名字相同的课程文件
-        for(int i=0;i<y.size();i++)
-        {
-            if(y[i].substr(0,tea_name.size())==tea_name)    //确认是查找的老师
-            {
-                vector<Course> temp_cou;
-                copyAllInfo("Course//"+cou_name+"//"+y[i]+".dat",temp_cou);
-                x[cou_name+"_"+y[i]]=temp_cou;
-            }
-        }
-        return 1;
-    }
-
-
-
+//修改学生的总评成绩
+//修改成功（找到student）返回1否则返回0
+int TeaOp::changeScore(vector<Student>s,double score) {
+	if (s.empty())return 0;
+	vector<string>s_course = getStuCourse(s[0]);
+	int i;
+	for (i = 0;i < s_course.size();i++) {
+		int index = s_course[i].find_first_of("/", 0);
+		string c_name = s_course[i].substr(0, index);
+		if (c_name == cou_name) {
+			s_course[i] = cou_name + "/" + to_string(score);
+			break;//如果课已经存于学生中，直接修改
+		}
+	}
+	if (i == s_course.size())s_course.push_back(cou_name + "/" + to_string(score));
+	s[0].course = "";
+	for (auto c : s_course)s[0].course += c;//修改学生对象里的course
+	modifyInfo(s[0].number, Stu_number, s[0]);//修改学生文件
+	return 1;
+}
