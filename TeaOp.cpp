13731c86598625b833b43ca89afacb563fd1b820
thousandyoung <67034123+thousandyoung@users.noreply.github.com>
2021-03-15 23:57:31 +0800

    Update TeaOp.cpp
    修改了teaop string->char[]

--- TeaOp.cpp
+++ TeaOp.cpp
@@ -2,11 +2,11 @@
 #include<cstring>
 #include<sstream>
 struct cou_info {
-	string cou_name;		//课程名字
-	string class_name;		//教学班名
-	string tea_name;		//授课老师姓名
-	string position;		//授课地点
-	string time;			//授课时间
+	char cou_name[20];		//课程名字
+	char class_name[20];		//教学班名
+	char tea_name[20];		//授课老师姓名
+	char position[20];		//授课地点
+	char time[20];			//授课时间
 	double propotion;		//平时成绩比例
 }cur_course;
 //登录老师账号
@@ -40,7 +40,10 @@ vector<Course> TeaOp::Tea_ReadClassScore() {
 //计算平均分
 double TeaOp::getAverageScore(const vector<Course>&t) {
 	double all_score = 0;
-	for (auto i : t)all_score += atof(i.final_grade.c_str());
+	for (auto i : t){
+		string temp = i.final_grade;
+		all_score += atof(temp.c_str());
+	}
 	return all_score / t.size();
 }
 //读出course文件第一行的课程信息
@@ -63,12 +66,12 @@ int TeaOp::InputScore(string number,double score1,double score2) {
 	vector<Student>s;
 	if (copyInfo(number, Stu_number, s)==0)return 0;
 	string cur_filename = "course/" + cur_course.cou_name + "/" + cur_course.class_name + ".dat";
-	Course temp(s[0].name, s[0].number, to_string(score1), to_string(score2), to_string(score1 * cur_course.propotion + (1 - cur_course.propotion) * score2));
+	Course temp(s[0].name, s[0].number, to_string(score1).c_str(), to_string(score2).c_str(), to_string(score1 * cur_course.propotion + (1 - cur_course.propotion) * score2).c_str());
 	fstream f(cur_filename, ios::binary | ios::out | ios::in);
 	Course stu_temp;
 	while (f.read((char*)&stu_temp,sizeof(stu_temp)))
 	{
-		if (stu_temp.stu_number == number) {
+		if (stu_temp.stu_number == number.data()) {
 			f.seekp(long(f.tellg()) - sizeof(stu_temp));
 			f.write((char*)&temp, sizeof(temp));
 		}
@@ -89,12 +92,12 @@ int TeaOp::changeScore(vector<Student>s,double score) {
 	for (i = 0;i < s_course.size();i++) {
 		int index = s_course[i].find_first_of("/", 0);
 		string c_name = s_course[i].substr(0, index);
-		if (c_name == cur_course.cou_name) {
-			s_course[i] = cur_course.cou_name + "/" + to_string(score);
+		if (c_name.data() == cur_course.cou_name) {
+			s_course[i] = string(cur_course.cou_name) + "/" + to_string(score);
 			break;//如果课已经存于学生中，直接修改
 		}
 	}
-	if (i == s_course.size())s_course.push_back(cur_course.cou_name + "/" + to_string(score));
+	if (i == s_course.size())s_course.push_back( string(cur_course.cou_name) + "/" + to_string(score));
 	s[0].course = "";
 	for (auto c : s_course)s[0].course += c;//修改学生对象里的course
 	modifyInfo(s[0].number, Stu_number, s[0]);//修改学生文件
