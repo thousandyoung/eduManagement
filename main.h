aba1f1ea94d24ef9f0fc3e373cc4f24244de9afc
paopaochahu09 <79733950+paopaochahu09@users.noreply.github.com>
2021-04-05 15:15:01 +0800

    Update main.h

--- main.h
+++ main.h
@@ -17,93 +17,123 @@
 #include<map>
 
 using namespace std;
-
+/*
+//在cpp文件已经定义索引对象
 multimap<string, string>Stu_number;
 multimap<string, string>Stu_name;
 multimap<string, string>Tea;
 multimap<string, string>Ad;
+*/
 //---------------------------------------各类定义-----------------------------------------
 class Course       //存储在课程文件中的课程类
 {
 public:
 
-	string stu_name;      //学生姓名
-	string stu_number;    //学生学号
-	string usual_grade;         //平时成绩
-	string final_grade;        //期末成绩
-	string overall_grade;       //总评成绩
+	char stu_name[20];      //学生姓名
+	char stu_number[20];    //学生学号
+	char usual_grade[20];         //平时成绩
+	char final_grade[20];        //期末成绩
+	char overall_grade[20];       //总评成绩
 	Course(){}
-	Course(string stu_name,string stu_number,string usual_grade,string final_grade,string overall_grade):stu_name(stu_name),stu_number(stu_number),usual_grade(usual_grade),final_grade(final_grade),overall_grade(overall_grade){}
+	Course(char* stu_name,char* stu_number,char* usual_grade,char* final_grade,char* overall_grade){
+		Copy(stu_name,this->stu_name),Copy(stu_number,this->stu_number),Copy(usual_grade,this->usual_grade),Copy(final_grade,this->final_grade),Copy(overall_grade,this->overall_grade);
+	}
 };
 
 class User {
 public:
-	string number;      //学号 只能用学号或者工号登录
-	string password;
+	char number[20];      //学号 只能用学号或者工号登录
+	char password[20];
 };
 
 class Student : public User
 {
 public:
-	string name;
-	string grade;    //年级
-	string college;  //学院
-	string major;    //专业
-	string Class;    //班级
-	string course;   //学生全部课程信息。以 课程名字/总评成绩/课程名字/总评成绩...
-	Student() { ; }
-	Student(string Number, string Password, string Name, string Grade, string College, string Major, string course = "\0")
+	char name[20];
+	char grade[20];    //年级
+	char college[20];  //学院
+	char major[20];    //专业
+	char Class[20];    //班级
+	char course[100];   //学生全部课程信息。以 课程名字/总评成绩/课程名字/总评成绩...
+	Student() { number[0]=name[0]=password[0]=grade[0]=college[0]=major[0]=Class[0]=course[0]='\0'; }
+	Student(const Student& x)
 	{
-		number = Number, password = Password, name = Name, grade = Grade, college = College, major = Major, this->Class = Class, this->course = course;
+		Copy(x.name,name),Copy(x.number,number),Copy(x.course,course),Copy(x.password,password);
+		Copy(x.college,college),Copy(x.Class,Class),Copy(x.major,major),Copy(x.grade,grade);
 	}
-	void transCourse(const Course& x,string cou_name,string class_name);     //将课程类对象信息提取出来，并存在course数据中
-	void set_number(string number) { this->number = number; }
-	void set_pass(string password) { this->password = password; }
-	void set_name(string name) { this->name = name; }
-	void set_grade(string grade) { this->grade = grade; }
-	void set_major(string major) { this->major = major; }
-	void set_course(string course) { this->course = course; }
-	void set_college(string college) { this->college = college; }
-	void set_class(string Class) { this->Class = Class; }
+	Student(char* Number, char* Password, char* Name, char* Grade, char* College,char *Class, char* Major, char* course = "\0")
+	{
+		Copy(Number,number),Copy(Password,password),Copy(Name,name),Copy(Grade,grade),Copy(College,college),Copy(Major,major),Copy(Class,this->Class),Copy(course,this->course);
+	}
+	void transCourse( Course& x,char* cou_name,char* class_name);     //将课程类对象信息提取出来，并存在course数据中
+	void set_number(char* number) { Copy(number,this->number ); }
+	void set_pass(char* password) { Copy(password,this->password); }
+	void set_name(char* name) { Copy(name,this->name); }
+	void set_grade(char* grade) { Copy(grade,this->grade); }
+	void set_major(char* major) { Copy(major,this->major); }
+	void set_course(char* course) { Copy(course,this->course); }
+	void set_college(char* college) { Copy(college,this->college); }
+	void set_class(char* Class) { Copy(Class,this->Class); }
 };
 
-class Teacher : public User
+class Teacher :public User
 {
 public:
-	string name;   //姓名
-	string course;    //所教课程名字的集合 格式为 课程名字&课程名字&...
-	Teacher() { ; }
-	Teacher(string Number, string Password, string Name, string course = "\0")
+	char name[20];
+	char course[100];
+
+	Teacher() { name[0]=course[0]=number[0]=password[0]='\0'; }
+	Teacher(char *Number, char* Password, char* Name, char* course = "\0")
 	{
-		number = Number, password = Password, name = Name, this->course = course;
+		Copy(Number,number), Copy(Password,password), Copy(Name,name), Copy(course,this->course);
+	}
+	Teacher(const Teacher &x)
+	{
+		Copy(x.name,name),Copy(x.number,number),Copy(x.course,course),Copy(x.password,password);
 	}
 	//void transCourse(const string& x) { course += x + "&"; }     //将课程类对象信息提取出来，并存在course数据中
-	void set_number(string number) { this->number = number; }
-	void set_pass(string password) { this->password = password; }
-	void set_name(string name) { this->name = name; }
-	void set_course(string course) { this->course = course; }
+	void set_number(char* number) { Copy(number,this->number); }
+	void set_pass(char* password) { Copy(password,this->password); }
+	void set_name(char* name) { Copy(name,this->name); }
+	void set_course(char* course) { Copy(course,this->course); }
 };
 
 class Admini :public User      //教务员
 {
 public:
-	string name;        //姓名
+	char name[20];        //姓名
+	Admini(){name[0]='\0',password[0]=number[0]='\0';}
+	Admini(char *name)
+	{
+		Copy(name,this->name);
+	}
+	Admini(const Admini & x)
+	{
+		Copy(x.name,name),Copy(x.number,number),Copy(x.password,password);
+	}
+	void set_name(char *name)
+	{
+		Copy(name,this->name);
+	}
 };
 
 class Find       //索引类键值对
 {
 public:
-	string key;
-	string value;
-	Find() {}
-	Find(string a, string b):key(a),value(b){}
+	char key[20];
+	char value[40];
+	Find() {key[0]=value[0]='\0';}
+	Find(char *key,char* value)
+	{
+		Copy(key,this->key),Copy(value,this->value);
+	}
 };
 struct cou_info {
-	string cou_name;		//课程名字
-	string class_name;		//教学班名
-	string tea_name;		//授课老师姓名
-	string position;		//授课地点
-	string time;			//授课时间
+	char cou_name[20];		//课程名字
+	char class_name[20];		//教学班名
+	char tea_name[20];		//授课老师姓名
+	char position[20];		//授课地点
+	char time[20];			//授课时间
 	double propotion;		//平时成绩比例
 }cur_course;
 //-----------------------------------------功能函数-----------------------------------
